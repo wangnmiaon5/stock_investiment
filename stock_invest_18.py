468850106db71add64be1c81afad8209578a68ab
Miao <wangnmiaon5@gmail.com>
2018-03-14 11:17:41 -0700

    update py file

--- stock_invest_18.py
+++ stock_invest_18.py
@@ -2,20 +2,6 @@
 # 2018 stock investment
 ################################################
 
-from yahoo_finance import Share
-
-dir(Share)
-
-yahoo = Share('GILD')
-print (yahoo.get_open())
-print (yahoo.get_price())
-print (yahoo.get_trade_datetime())
-
-
-from yahoo_finance import Share as sh 
-st = sh ('NFLX') 
-print (st.get_historical('2017-05-01', '2017-05-12'))
-
 
 ################ how to download repository from github then install into python
 """
@@ -29,22 +15,59 @@ import os
 import numpy as np
 import pandas as pd
 import datetime
+from pandas_datareader import data
 
 
 os.chdir('/Users/miaowang/Box Sync/2017DS/stock_investment_2018')
 os.chdir('/Users/yipan/Desktop/stock_investment')
 
+############################################
+#### get the stock price from google finance
+############################################
+tickers = ['AMTD']
+# Define which online source one should use
+data_source = 'google'
+start_date = '2008-01-01'
+end_date = '2018-3-10'
+panel_data = data.DataReader(tickers, data_source, start_date, end_date)
+# Getting just the adjusted closing prices. This will return a Pandas DataFrame
+# The index in this DataFrame is the major index of the panel_data.
+close = panel_data.ix['Close']
+# Getting all weekdays between 01/01/2000 and 12/31/2016
+all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
+# All we need to do is reindex close using all_weekdays as the new index
+close = close.reindex(all_weekdays)
+def get_year(x):
+    return x.date().year
+close['date'] = close.index
+close['year'] = close['date'].apply(get_year)
+price = close.groupby(['year']).median()
+price.index = pd.PeriodIndex(price.index, freq='A-SEP')
+
+
+############################################
+#### get fundamental data from gm package
+############################################
 import good_morning as gm
 dir(gm)
 kr = gm.KeyRatiosDownloader()
 kr_frames = kr.download('AMTD')
-data1 = kr_frames[0]
-
-
+data1 = kr_frames[0].T
+data1.index = data1.index.values.astype(str)
 data2 = kr_frames[1].iloc[kr_frames[1].index.get_loc('EBT Margin')]
-data2.index.values.tolist()
-data2.iloc[i]['EBT Margin']
+data2.index = data2.index.values.astype(str)
 
+data = data1.join(price, how='outer')
+price.index = pd.PeriodIndex(start='2008', end='2018', freq='A')
+
+
+def join_dfs(ldf, rdf):
+    return ldf.join(rdf, how='inner')
+
+final_df = reduce(join_dfs, data) #that's the magic
+final_df.head()
+
+data.T
 data2.index
 month['Fund'] = month.index
 
@@ -66,12 +89,17 @@ data1 = kr_frames[0].T[['Book Value Per Share * USD',
                  'Dividends USD',
                  'Earnings Per Share USD', 
                  'Shares Mil',
-                 'Payout Ratio % *']]
+                 'Payout Ratio % *',
+                 'EBT Margin']]
+
+
+pd.concat(data1,data2)
 
 
 
+pieces = [df[:3], df[3:7], df[7:]]
 
-
+In [4]: concatenated = concat(pieces)
 
 
 df = df.assign(delta_A=np.zeros(len(df.A)))
@@ -95,37 +123,10 @@ data.T
 kr2 = gm.FinancialsDownloader()
 kr_fins = kr2.download('AMTD')
 
-from pandas_datareader import data
-
-from pandas_datareader import data
-import pandas as pd
 
 
-# Define the instruments to download. We would like to see Apple, Microsoft and the S&P500 index.
-tickers = ['AMTD']
-
-# Define which online source one should use
-data_source = 'google'
-
-# We would like all available data from 01/01/2000 until 12/31/2016.
-start_date = '2000-01-01'
-end_date = '2018-3-10'
-
-# User pandas_reader.data.DataReader to load the desired data. As simple as that.
-panel_data = data.DataReader(tickers, data_source, start_date, end_date)
+month = perf.groupby(['Fund', '1M breach']).size().reset_index(name='counts').pivot(index = 'Fund', columns = '1M breach', values = 'counts')
 
 
-# Getting just the adjusted closing prices. This will return a Pandas DataFrame
-# The index in this DataFrame is the major index of the panel_data.
-close = panel_data.ix['Close']
-
-# Getting all weekdays between 01/01/2000 and 12/31/2016
-all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
-
-# How do we align the existing prices in adj_close with our new set of dates?
-# All we need to do is reindex close using all_weekdays as the new index
-close = close.reindex(all_weekdays)
-
-close.describe()
 
 
